2bf46ea40d528337775c8e37a13916e8463ee4ae
aminebenichou <aamine060@gmail.com>
2026-07-04 17:43:07 +0100

    sync

--- notes/app/components/CustomButton.tsx
+++ notes/app/components/CustomButton.tsx
@@ -1,24 +1,60 @@
-
+'use client'
 
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 interface ButtonProps {
     Text: string
-    Prefix?: any 
-    onClick?: ()=> void
-}   
+    Prefix?: any
+    Suffix?: any
+    ClickedSuffix?: any
+    onClick?: () => void
+    isfolder?: boolean
+    folders?: any
+}
 
 
-export default function CustomButton(props:ButtonProps) {
+export default function CustomButton(props: ButtonProps) {
+
+    const [expand, setExpand] = useState(false)
+    // useEffect(()=>{
+    //     expand ? 
+    // }, [expand])
+
     return (
         <div>
-            <Button className="flex cursor-pointer bg-transparent hover:bg-gray-800 hover:text-white rounded-md text-gray-900 m-1 text-lg p-5">
+            {!props.isfolder ? <Button className="flex cursor-pointer bg-transparent hover:bg-gray-800 hover:text-white rounded-md text-gray-900 m-1 text-lg p-5">
 
                 {props.Prefix}
                 <span>
                     {props.Text}
                 </span>
-            </Button>
+            </Button> : <div>
+                <Button onClick={() => setExpand(!expand)} className="flex justify-between w-56 cursor-pointer bg-transparent hover:bg-gray-800 hover:text-white rounded-md text-gray-900 m-1 text-lg p-5">
+                    <div className="flex items-center">
+                    {props.Prefix}
+                    <span className="ml-2">
+                        {props.Text}
+                    </span>
+
+                    </div>
+                    <div>
+                        {expand ? props.ClickedSuffix : props.Suffix}
+                    </div>
+
+                </Button>
+                {expand && <div className="pl-7">
+                    {
+                        props.folders?.map((folder:any) => <Button key={folder.text} className="flex cursor-pointer bg-transparent hover:bg-gray-800 hover:text-white rounded-md text-gray-900 m-1 text-lg p-5">
+
+                            
+                            <span>
+                                {folder.text}
+                            </span>
+                        </Button>)
+                    }
+                </div>}
+            </div>}
         </div>
     )
 }
--- notes/app/page.tsx
+++ notes/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import CustomButton from "./components/CustomButton";
-import { ArchiveIcon, CirclePlus, SearchIcon } from "lucide-react";
+import { ArchiveIcon, ArrowDownIcon, ArrowUpIcon, CirclePlus, FolderIcon, FoldersIcon, SearchIcon } from "lucide-react";
 
 
 
@@ -18,13 +18,26 @@ export default function Home() {
     { text: "Create Note", icon: <CirclePlus />, onclick: ()=>AddNote() },
     { text: "Search", icon: <SearchIcon /> },
     { text: "Archives", icon: <ArchiveIcon /> },
+    { text: "Folder", icon: <FoldersIcon />, suffix:<ArrowDownIcon />,clickedSuffix:<ArrowUpIcon />, isFolder:true, folders:[
+      {
+        text:'Finance'
+      },
+      {
+        text:'Todo'
+      },
+      {
+        text:'Shopping'
+      },
+    ] },
+  
+  
   ]
 
   return (
     <div>
       <div>
         {
-          navButtons.map((btn) => <CustomButton key={btn.text} Text={btn.text} Prefix={btn.icon} onClick={btn.onclick} />)
+          navButtons.map((btn) => <CustomButton key={btn.text} Text={btn.text} Prefix={btn.icon} onClick={btn.onclick} isfolder={btn.isFolder || false} folders={btn.folders} Suffix={btn.suffix} ClickedSuffix={btn.clickedSuffix} />)
         }
       </div>
     </div>
